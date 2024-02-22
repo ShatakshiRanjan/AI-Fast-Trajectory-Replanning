a6f66475f19ac3c42d2198c37637261ea4f40858
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-02-22 01:08:34 -0500

    Refactor maze generation functions

--- Project files/maze.py
+++ Project files/maze.py
@@ -2,32 +2,32 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-def generate_maze_with_start_end(num_rows, num_cols):
+def generateMaze(num_rows, num_cols):
     maze = np.zeros((num_rows, num_cols), dtype=int)
     visited = set()
     stack = []
 
-    def is_valid_cell(row, col):
+    def checkValid(row, col):
         return 0 <= row < num_rows and 0 <= col < num_cols
 
-    def get_unvisited_neighbors(row, col):
+    def unvisited(row, col):
         neighbors = []
         for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nr, nc = row + dr, col + dc
-            if is_valid_cell(nr, nc) and (nr, nc) not in visited:
+            if checkValid(nr, nc) and (nr, nc) not in visited:
                 neighbors.append((nr, nc))
         return neighbors
 
-    def mark_as_blocked_or_unblocked(row, col):
+    def mark(row, col):
         if random.random() < 0.3:
             maze[row, col] = 1  # Blocked
         else:
             maze[row, col] = 0  # Unblocked
 
-    def backtrack_to_unvisited_cell():
+    def backtrack():
         while stack:
             row, col = stack.pop()
-            unvisited_neighbors = get_unvisited_neighbors(row, col)
+            unvisited_neighbors = unvisited(row, col)
             if unvisited_neighbors:
                 return unvisited_neighbors[0]
         return None
@@ -50,14 +50,14 @@ def generate_maze_with_start_end(num_rows, num_cols):
         row, col = stack[-1]
         visited.add((row, col))
 
-        unvisited_neighbors = get_unvisited_neighbors(row, col)
+        unvisited_neighbors = unvisited(row, col)
         if unvisited_neighbors:
             next_row, next_col = unvisited_neighbors[random.randint(0, len(unvisited_neighbors) - 1)]
             visited.add((next_row, next_col))
             stack.append((next_row, next_col))
-            mark_as_blocked_or_unblocked(next_row, next_col)
+            mark(next_row, next_col)
         else:
-            next_cell = backtrack_to_unvisited_cell()
+            next_cell = backtrack()
             if next_cell:
                 stack.append(next_cell)
             else:
@@ -69,22 +69,24 @@ def generate_maze_with_start_end(num_rows, num_cols):
 
     return maze, (start_row, start_col), (end_row, end_col)
 
-def generate_multiple_mazes_with_start_end(num_mazes, maze_size):
-    mazes = [generate_maze_with_start_end(maze_size, maze_size) for _ in range(num_mazes)]
+def generateMultiple(num_mazes, maze_size):
+    mazes = [generateMaze(maze_size, maze_size) for _ in range(num_mazes)]
     return mazes
 
+# https://matplotlib.org/stable/
 def visualize_maze(maze):
     plt.imshow(maze, cmap='binary', origin='lower')
     plt.colorbar()
     plt.show()
 
+# https://numpy.org/doc/stable/reference/generated/numpy.savetxt.html
 def save_maze_to_file(maze, filename):
     np.savetxt(filename, maze, fmt='%d', delimiter=',')
 
 # Generate 50 grid world environments with unique start and end points
-num_mazes = 2
-maze_size = 5
-mazes_with_start_end = generate_multiple_mazes_with_start_end(num_mazes, maze_size)
+num_mazes = 2  # Number of mazes to generate
+maze_size = 5  # Size of the maze 
+mazes_with_start_end = generateMultiple(num_mazes, maze_size)
 
 # Visualize and save the first maze with start and end points
 visualize_maze(mazes_with_start_end[0][0])
