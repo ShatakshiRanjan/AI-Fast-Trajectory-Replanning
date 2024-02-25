b7e3e6a6e94921340e9c59466b28a221be03f8a7
sahana-s17 <125157168+sahana-s17@users.noreply.github.com>
2024-02-25 17:54:33 -0500

    Update experiments.py

--- Project files/experiments.py
+++ Project files/experiments.py
@@ -1,12 +1,12 @@
-# from astar import calc
-
 import matplotlib.pyplot as plt
 import numpy as np
+from adaptive_a_star import adaptive_a_star
 from astar import a_star
+from backward_astar import backward_astar
 from maze import generateMaze
 
 
-def calc(maze):
+def calcAStar(maze):
     # Generate maze using generateMaze from the maze module
 
     # Run A* with ties broken in favor of smaller g-values
@@ -28,8 +28,50 @@ def calc(maze):
     return gs_expanded_nodes, gg_expanded_nodes, path_smaller_g, path_larger_g
 
 
+def calcBAStar(maze):
+    # Run backward A* with ties broken in favor of larger g-values
+
+    path_larger_g_a, expanded_nodes_a = a_star(maze, break_ties_smaller_g=False)
+
+    print(
+        "Repeated A* path found with ties broken for larger g-values:", path_larger_g_a
+    )
+    print("Repeated A* - number of expanded nodes", expanded_nodes_a)
+
+    path_larger_g_b, expanded_nodes_b = backward_astar(maze, break_ties_smaller_g=False)
+
+    print(
+        "Backward A* path found with ties broken for larger g-values:", path_larger_g_b
+    )
+    print("Backward A* - number of expanded nodes", expanded_nodes_b)
+
+    return path_larger_g_a, path_larger_g_b, expanded_nodes_a, expanded_nodes_b
+
+
+def calcAAStar(maze):
+    # Run Adaptive A* with ties broken in favor of larger g-values
+
+    path_larger_g_a, expanded_nodes_a = a_star(maze, break_ties_smaller_g=False)
+
+    print(
+        "Repeated A* path found with ties broken for larger g-values:", path_larger_g_a
+    )
+    print("Repeated A* - number of expanded nodes", expanded_nodes_a)
+
+    path_larger_g_d, expanded_nodes_d = adaptive_a_star(
+        maze, break_ties_smaller_g=False
+    )
+
+    print(
+        "Adaptive A* path found with ties broken for larger g-values:", path_larger_g_a
+    )
+    print("Adaptive A* - number of expanded nodes", expanded_nodes_d)
+
+    return path_larger_g_a, path_larger_g_d, expanded_nodes_a, expanded_nodes_d
+
+
 mazes = []
-for i in range(0, 50):  
+for i in range(0, 50):  # 50 mazes
     maze, _, _ = generateMaze(5, 5)
     mazes.append(maze)
 
@@ -40,8 +82,8 @@ def testAstar():  # returns mean # expanded nodes for g smaller and for g greate
     plt.rcParams.update({"axes.titlesize": "medium"})
     fig_height = 6 * len(mazes)
     fig, ax = plt.subplots(len(mazes), 2, figsize=(10, fig_height))
-    for i in range(0, len(mazes)):  # two mazes
-        gs_exp_nodes, gg_exp_nodes, path_smaller_g, path_larger_g = calc(mazes[i])
+    for i in range(0, len(mazes)):
+        gs_exp_nodes, gg_exp_nodes, path_smaller_g, path_larger_g = calcAStar(mazes[i])
 
         if path_smaller_g and path_larger_g:
 
@@ -64,11 +106,86 @@ def testAstar():  # returns mean # expanded nodes for g smaller and for g greate
         gg_sum += gg_exp_nodes
 
     fig.tight_layout()
-    plt.show()
+    plt.savefig("astar.png")
     return gs_sum / len(mazes), gg_sum / len(mazes)
+
+
+def testBackwardAstar():
+    gg_sum_a = 0
+    gg_sum_b = 0
+    plt.rcParams.update({"axes.titlesize": "medium"})
+    fig_height = 6 * len(mazes)
+    fig, ax = plt.subplots(len(mazes), 2, figsize=(10, fig_height))
+    for i in range(0, len(mazes)):
+        path_larger_g_a, path_larger_g_b, expanded_nodes_a, expanded_nodes_b = (
+            calcBAStar(mazes[i])
+        )
+        if path_larger_g_a and path_larger_g_b:
+            #
+            ax[i, 0].set_title("Forward A* path with ties broken for larger g-values")
+            im = ax[i, 0].imshow(mazes[i], cmap="binary", origin="lower")
+            for node in path_larger_g_a:
+                ax[i, 0].plot(node[1], node[0], "ro")
+            fig.colorbar(im, ax=ax[i, 0], orientation="vertical")
+
+            ax[i, 1].set_title("Backward A* path with ties broken for larger g-values")
+            im2 = ax[i, 1].imshow(mazes[i], cmap="binary", origin="lower")
+            for node in path_larger_g_b:
+                ax[i, 1].plot(node[1], node[0], "ro")
+            fig.colorbar(im2, ax=ax[i, 1], orientation="vertical")
+
+        else:
+            print("No path found for at least one version of A*.")
+
+        gg_sum_a += expanded_nodes_a
+        gg_sum_b += expanded_nodes_b
+
+    fig.tight_layout()
+    plt.savefig("backward_a_star.png")
+    return gg_sum_a / len(mazes), gg_sum_b / len(mazes)
+
+
+def testAdaptiveAstar():
+    gg_sum_a = 0
+    gg_sum_d = 0
+    plt.rcParams.update({"axes.titlesize": "medium"})
+    fig_height = 6 * len(mazes)
+    fig, ax = plt.subplots(len(mazes), 2, figsize=(10, fig_height))
+    for i in range(0, len(mazes)):
+        path_larger_g_a, path_larger_g_d, expanded_nodes_a, expanded_nodes_d = (
+            calcAAStar(mazes[i])
+        )
+        if path_larger_g_a and path_larger_g_d:
+            ax[i, 0].set_title("Forward A* path with ties broken for larger g-values")
+            im = ax[i, 0].imshow(mazes[i], cmap="binary", origin="lower")
+            for node in path_larger_g_a:
+                ax[i, 0].plot(node[1], node[0], "ro")
+            fig.colorbar(im, ax=ax[i, 0], orientation="vertical")
+
+            ax[i, 1].set_title("Adaptive A* path with ties broken for larger g-values")
+            im2 = ax[i, 1].imshow(mazes[i], cmap="binary", origin="lower")
+            for node in path_larger_g_d:
+                ax[i, 1].plot(node[1], node[0], "ro")
+            fig.colorbar(im2, ax=ax[i, 1], orientation="vertical")
+
+        else:
+            print("No path found for at least one version of A*.")
+
+        gg_sum_a += expanded_nodes_a
+        gg_sum_d += expanded_nodes_d
+
+    fig.tight_layout()
+    plt.savefig("adaptive_a_star.png")
+    return gg_sum_a / len(mazes), gg_sum_d / len(mazes)
 
 
 if __name__ == "__main__":
     gs_avg, gg_avg = testAstar()
     print("G smaller average expanded nodes: ", gs_avg)
     print("G larger average expanded nodes: ", gg_avg)
+    a_avg, b_avg = testBackwardAstar()
+    print("A star average expanded nodes: ", a_avg)
+    print("Backward A star average expanded nodes: ", b_avg)
+    a_avg, d_avg = testAdaptiveAstar()
+    print("A star average expanded nodes: ", a_avg)
+    print("Adaptive A star average expanded nodes: ", d_avg)
